9cf4f67ece92b4cadea9a8e3826a77935dda376d
Abolfazl Amiri <aa.smpro@gmail.com>
2022-01-16 00:23:58 +0330

    feat: new Hero

--- src/utils/randomChar.js
+++ src/utils/randomChar.js
@@ -3,7 +3,8 @@ export const lower = "abcdefghijklmnopqrstuvwxyz";
 export const number = "0123456789";
 export const signs = "!@#$%&*:;<>{}[]()";
 
-export const allChars = upper + lower + number + signs;
+// export const allChars = upper + lower + number + signs;
+export const allChars = upper + number;
 
 export const randomChar = (chars = allChars) => {
   return chars.charAt(Math.floor(Math.random() * chars.length));
