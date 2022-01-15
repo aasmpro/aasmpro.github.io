94b24f2b67ca9252179bf721ab4381951cb5adf1
Abolfazl Amiri <aa.smpro@gmail.com>
2022-01-16 00:19:50 +0330

    feat: new resume

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -11,21 +11,21 @@ export const Projects = () => {
         <div className="mb-5 p-4 border-dark-0 border-b-2">
           <div className="flex justify-between">
             <Link
-              className="px-2 bg-light-1 rounded-full text-dark-0 hover:text-light-0
+              className="px-3 py-0.5 bg-light-1 rounded-full text-dark-0 hover:text-light-0
               transition duration-300  hover:bg-black hover:ring-2 hover:ring-light-1"
               to="/">
               Home
             </Link>
             {showDetails ? (
               <span
-                className="px-2 bg-light-1 rounded-full text-dark-0 hover:text-light-0
+                className="px-3 py-0.5 bg-light-1 rounded-full text-dark-0 hover:text-light-0
             transition duration-300  hover:bg-black hover:ring-2 hover:ring-light-1 cursor-pointer"
                 onClick={() => setShowDetails(false)}>
                 Details
               </span>
             ) : (
               <span
-                className="px-2 bg-dark-0 rounded-full text-light-0 hover:text-light-0
+                className="px-3 py-0.5 bg-dark-0 rounded-full text-light-0 hover:text-light-0
             transition duration-300  hover:bg-black hover:ring-2 hover:ring-dark-0 cursor-pointer"
                 onClick={() => setShowDetails(true)}>
                 Details
--- src/views/Resume.jsx
+++ src/views/Resume.jsx
@@ -21,13 +21,13 @@ export const Resume = () => {
         <div className="mb-5 p-4 border-dark-0 border-b-2 print:hidden">
           <div className="flex justify-between">
             <Link
-              className="px-2 bg-light-1 rounded-full text-dark-0 hover:text-light-0
+              className="px-3 py-0.5 bg-light-1 rounded-full text-dark-0 hover:text-light-0
               transition duration-300  hover:bg-black hover:ring-2 hover:ring-light-1"
               to="/">
               Home
             </Link>
             <a
-              className="px-2 bg-yellow-0 rounded-full
+              className="px-3 py-0.5 bg-yellow-0 rounded-full
               transition duration-300  hover:bg-black hover:ring-2 hover:ring-yellow-0"
               href="/AbolfazlAmiri.pdf">
               Download
@@ -35,78 +35,81 @@ export const Resume = () => {
           </div>
         </div>
         <div className="px-5 print:!p-0 divide-y-2 divide-dark-0">
-          <div className="pb-4">
-            <p className="print:text-3xl text-3xl font-bold">Abolfazl Amiri</p>
-            <p className="print:text-xl text-xl">Python & Web Developer</p>
-            <div className="mt-3 flex flex-col print:text-sm sm:flex-row">
-              <div className="sm:w-4/12">
-                <p className="mb-2">
+          <div className="pb-4 flex flex-col justify-between md:flex-row print:flex-row">
+            <div>
+              <p className="print:text-4xl text-4xl font-bold">
+                Abolfazl Amiri
+              </p>
+              <p className="print:text-2xl text-2xl">Python & Web Developer</p>
+            </div>
+            <div>
+              <div className="flex flex-col print:text-sm whitespace-nowrap">
+                <div className="mb-2 print:mb-1.5">
                   <a
-                    className="px-3 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
+                    className="block max-w-[243px] px-3 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
                             transition duration-300 hover:bg-black hover:ring-2 hover:ring-blue-0"
                     href="https://abolfazlamiri.ir"
                     title="Webpage">
-                    <i class="fas fa-globe-americas text-light-0" />{" "}
+                    <i class="fas fa-globe-americas text-light-0 mr-1.5" />{" "}
                     abolfazlamiri.ir
                   </a>
-                </p>
-                <p>
+                </div>
+                <div className="mb-2 print:mb-1.5">
                   <a
-                    className="px-3 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
-                            transition duration-300 hover:bg-black hover:ring-2 hover:ring-blue-0"
-                    href="tel:+989217214929"
-                    title="Phone">
-                    <i class="fas fa-phone text-light-0" /> +989217214929
-                  </a>
-                </p>
-              </div>
-              <div>
-                <p className="mb-2 mt-2 sm:mt-0">
-                  <a
-                    className="px-3 py-0.5 font-bold bg-red-0 rounded-full print:text-light-0
+                    className="block max-w-[243px] px-3 py-0.5 font-bold bg-red-0 rounded-full print:text-light-0
                             transition duration-300 hover:bg-black hover:ring-2 hover:ring-red-0"
                     href="mailto:aasmpro@gmail.com"
                     title="Email">
-                    <i class="fas fa-at text-light-0" /> aasmpro@gmail.com
+                    <i class="fas fa-at text-light-0 mr-1.5" />{" "}
+                    aasmpro@gmail.com
                   </a>
-                </p>
-                <p>
+                </div>
+                <div className="mb-2 print:mb-1.5">
                   <a
-                    className="px-4 mr-2 py-0.5 font-bold bg-dark-2 rounded-full print:text-light-0
+                    className="block max-w-[243px] px-3 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
+                            transition duration-300 hover:bg-black hover:ring-2 hover:ring-blue-0"
+                    href="tel:+989217214929"
+                    title="Phone">
+                    <i class="fas fa-phone text-light-0 mr-1.5" /> +989217214929
+                  </a>
+                </div>
+                <div>
+                  <a
+                    className="px-3.5 mr-2 py-0.5 font-bold bg-dark-2 rounded-full print:text-light-0
                             transition duration-300 hover:bg-black hover:ring-2 hover:ring-dark-2"
                     href="https://github.com/aasmpro"
                     title="Github">
                     <i class="fab fa-github-alt text-light-0" />
                   </a>
                   <a
-                    className="px-4 mr-2 py-0.5 font-bold bg-orange-0 rounded-full print:text-light-0
+                    className="px-3.5 mr-2 py-0.5 font-bold bg-orange-0 rounded-full print:text-light-0
                   transition duration-300 hover:bg-black hover:ring-2 hover:ring-orange-0"
                     href="https://stackoverflow.com/users/6484831/aasmpro"
                     title="Stackoverflow">
                     <i class="fab fa-stack-overflow text-light-0" />
                   </a>
                   <a
-                    className="px-4 mr-2 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
+                    className="px-3.5 mr-2 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
                   transition duration-300 hover:bg-black hover:ring-2 hover:ring-blue-0"
                     href="https://www.linkedin.com/in/aasmpro/"
                     title="Linkedin">
                     <i class="fab fa-linkedin-in text-light-0" />
                   </a>
                   <a
-                    className="px-4 mr-2 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
+                    className="px-3.5 mr-2 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
                   transition duration-300 hover:bg-black hover:ring-2 hover:ring-blue-0"
                     href="https://twitter.com/aasmpro"
                     title="Twitter">
                     <i class="fab fa-twitter text-light-0" />
                   </a>
                   <a
-                    className="px-4 mr-2 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
+                    className="px-3.5 py-0.5 font-bold bg-blue-0 rounded-full print:text-light-0
                   transition duration-300 hover:bg-black hover:ring-2 hover:ring-blue-0"
                     href="https://telegram.me/aasmpro/"
                     title="Telegram">
                     <i class="fab fa-telegram-plane text-light-0" />
                   </a>
-                </p>
+                </div>
               </div>
             </div>
           </div>
@@ -128,7 +131,7 @@ export const Resume = () => {
               relations with my team and help them to develop themselves. I'm a
               self-learner, without prejudice on tools and tech stacks, and
               ready for new challenges.
-              <ul class="list-disc pl-4 pt-2 grid md:grid-cols-2 print:sm:grid-cols-2">
+              <ul class="print:text-xs list-disc pl-4 pt-2 grid md:grid-cols-2 print:sm:grid-cols-2">
                 <li>Education: B.S. Computer Engineering</li>
                 <li>Marriage: Single</li>
                 <li>Military Service: Exempted</li>
@@ -155,24 +158,44 @@ export const Resume = () => {
                 .filter((exp) => exp.showExperiences)
                 .map((exp) => (
                   <div className="py-2 first:pt-0">
-                    <div>
-                      <p className="print:text-lg text-lg font-semibold">
-                        {exp.title}
-                      </p>
-                      <p className="print:text-base font-semibold">
-                        {exp.company}
-                      </p>
-                      <p className="print:text-xs text-sm font-semibold">
-                        {exp.startDate} - {exp.endDate} | {exp.type} |{" "}
-                        {exp.location}
-                      </p>
-                    </div>
-                    <div className="print:text-xs text-sm">
-                      {exp.stack.map((stack) => (
-                        <span className="text-green-1 print:text-green-0 font-semibold">
-                          {stack},{" "}
-                        </span>
-                      ))}
+                    <div className="flex justify-between">
+                      <div>
+                        <p className="print:text-lg text-lg font-semibold">
+                          {exp.title}
+                        </p>
+                        <p className="print:text-base font-semibold">
+                          {exp.company}
+                        </p>
+                        <div className="whitespace-nowrap sm:hidden">
+                          <p className="print:text-xs text-sm font-semibold">
+                            {exp.startDate} - {exp.endDate}
+                          </p>
+                          <p className="print:text-xs text-sm font-semibold">
+                            {exp.location}
+                          </p>
+                          <p className="print:text-xs text-sm font-semibold">
+                            {exp.type}
+                          </p>
+                        </div>
+                        <div className="print:text-xs text-sm">
+                          {exp.stack.map((stack) => (
+                            <span className="text-green-1 print:text-green-0 font-semibold">
+                              {stack},{" "}
+                            </span>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="text-right whitespace-nowrap hidden sm:block">
+                        <p className="print:text-xs text-sm font-semibold">
+                          {exp.startDate} - {exp.endDate}
+                        </p>
+                        <p className="print:text-xs text-sm font-semibold">
+                          {exp.location}
+                        </p>
+                        <p className="print:text-xs text-sm font-semibold">
+                          {exp.type}
+                        </p>
+                      </div>
                     </div>
                     <div className="mt-2 mb-2 text-justify">
                       {exp.about.map((line) => (
@@ -226,7 +249,7 @@ export const Resume = () => {
                 ))}
             </div>
           </div>
-          <div className="py-4 print:text-sm">
+          <div className="py-3 print:text-sm">
             <p className="mb-3 print:text-xl text-xl font-bold">Projects</p>
             <div>
               Check out my site & Github for projects.
