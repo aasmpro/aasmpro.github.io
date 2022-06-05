cb7124f80535eb1d2f7c3e0b398951d8452f45da
Abolfazl Amiri <aa.smpro@gmail.com>
2022-06-05 20:04:53 +0430

    feat: tailwindcss 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 module.exports = {
   mode: "jit",
-  purge: {
-    enabled: true,
-    content: [
-      "./src/data/*.js",
-      "./src/views/*.{js,jsx}",
-      "./src/components/*.{js,jsx}",
-      "./src/Main.jsx",
-      "./src/index.js",
-    ],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/data/*.js",
+    "./src/views/*.{js,jsx}",
+    "./src/components/*.{js,jsx}",
+    "./src/Main.jsx",
+    "./src/index.js",
+  ],
+  safelist: [],
+  darkMode: "media", // 'media' or 'class'
   theme: {
     colors: {
       black: "#000000",
