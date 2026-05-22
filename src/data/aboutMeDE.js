5d7b0c9666cf1fc0271d7082fe4ad5a81763e510
Ross <aasmpro@Rosss-MacBook-Pro.local>
2026-05-22 02:47:52 +0200

    feat: new resume

--- src/data/aboutMeDE.js
+++ src/data/aboutMeDE.js
@@ -1,7 +1,7 @@
 export const aboutMeDE = {
-  title: "Python & Web Entwickler",
+  title: "Senior Softwareentwickler",
   cover: [
-    "Hallo! Ich bin Senior Software Engineer mit 9+ Jahren Erfahrung, hauptsächlich mit Fokus auf Webentwicklung und der Leitung technischer Teams.",
+    "Hallo! Ich bin Senior Softwareentwickler mit 9+ Jahren Erfahrung, hauptsächlich mit Fokus auf Webentwicklung und der Leitung technischer Teams.",
     "Mein Ziel ist es, die User Experience zu verbessern und sinnvollen Mehrwert zu schaffen, indem ich in engem Kontakt mit Endnutzer:innen bleibe und gleichzeitig sauberen, skalierbaren Code liefere. Ich baue starke, authentische Beziehungen zu meinem Team auf und unterstütze dessen Wachstum und Weiterentwicklung. Ich bin Autodidakt, offen gegenüber Tools und Technologie-Stacks und stets bereit für neue Herausforderungen.",
   ],
   info: [
